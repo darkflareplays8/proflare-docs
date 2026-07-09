9f39bbaded09eb7e4ba3012c9fea0a06becf9497
ProfessionalFlare <saimuralibalmuri@gmail.com>
2026-07-09 06:55:51 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ export default defineConfig({
   integrations: [
     starlight({
       title: "ProFlare Docs",
+      customCss: ["./src/styles/custom.css"],
       social: [
         { icon: "github", label: "GitHub", href: "https://github.com/darkflareplays8" },
       ],
